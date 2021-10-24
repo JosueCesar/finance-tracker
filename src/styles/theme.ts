c31bf3fb240cdb0679dd060deca2f9fe04a5e320
Cerberus <mendonca.josue@gmail.com>
2021-10-24 01:13:09 -0300

    feat: added color palette

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -1,5 +1,25 @@
 export default {
   colors: {
-    primary: '#fff'
+    primary: '#00B4D8',
+    primaryDark: '#0077B6',
+    primaryLight: '#90E0EF',
+
+    success: '#12A454',
+    success_light: 'rgba(18, 164, 84, 0.5)',
+
+    attention: '#E83F5B',
+    attention_light: 'rgba(232, 63, 91, 0.5)',
+
+    shape: '#6C757D',
+
+    title: '#E9ECEF',
+    titleDark: '#000000',
+
+    text: '#CED4DA',
+    textDark: '#000000',
+
+    background: '#343A40',
+    backgroundDark: '#212529',
+    backgroundLight: '#495057',
   }
 }
